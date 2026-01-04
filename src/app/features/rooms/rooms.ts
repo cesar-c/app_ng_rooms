87975ef5f226822e191e031183c9ff2a43b0acf6
CESAR <campolescano@gmail.com>
2026-01-04 00:18:13 -0500

    chore: initial commit

---
+++ src/app/features/rooms/rooms.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngroom-rooms',
+  imports: [],
+  templateUrl: './rooms.html',
+  styleUrl: './rooms.css',
+})
+export class Rooms {
+
+}
---
+++ src/app/shared/components/atoms/button/button.ts
@@ -0,0 +1,17 @@
+import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'ngroom-button',
+  imports: [NgClass],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class NgRoomButton {
+  @Input() disabled = false;
+  @Input() customClasses = '';
+  @Input() variant: 'primary' | 'secondary' | 'danger' | 'text' = 'primary';
+  @Output() clicked = new EventEmitter<void>();
+
+  classes = computed(() => ['btn', `btn-${this.variant}`, this.customClasses]);
+}
