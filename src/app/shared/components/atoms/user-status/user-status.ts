ef012a3dc6785e85d00c9201d05def8ab6121996
CESAR <campolescano@gmail.com>
2026-01-13 20:19:59 -0500

    refactor: refactor html to login and room, and user-status component inited

---
+++ src/app/shared/components/atoms/user-status/user-status.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngroom-user-status',
+  imports: [],
+  templateUrl: './user-status.html',
+  styleUrl: './user-status.css',
+})
+export class UserStatus {
+
+}
