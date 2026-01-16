97aee9333a7d1b23faa2523678cb3fc3da227a94
CESAR <campolescano@gmail.com>
2026-01-16 02:48:58 -0500

    feat: init desing room

--- src/app/features/rooms/rooms.ts
+++ src/app/features/rooms/rooms.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 
 @Component({
   selector: 'ngroom-rooms',
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './rooms.html',
   styleUrl: './rooms.css',
 })
-export default class Rooms {}
+export default class Rooms {
+  totalItems = 16;
+
+  items = computed(() =>
+    Array.from({ length: this.totalItems }, (_, i) => ({ id: i + 1, name: `Room ${i + 1}` }))
+  );
+
+  messages = [
+    { user: 'Alice', text: 'Hello everyone' },
+    { user: 'Bob', text: 'Hi Alice!' },
+  ];
+}
